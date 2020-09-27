f2f831e06031703fe8c3ff75cc4c46957e3eb1ef
Sonu <sonu3660@gmail.com>
2020-09-27 21:13:34 +0530

    final submit

--- example/src/App.js
+++ example/src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super()
     let d = new Date()
     let month = '-' + (d.getMonth() + 1)
     let day = '-' + d.getDate()
